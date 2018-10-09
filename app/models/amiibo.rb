82dee38714983d951950e75bdaf97a3e0c47cc5d
Balache404 <morgancuell@shaw.ca>
2018-10-09 15:32:38 +0000

    Validation added to models

--- app/models/amiibo.rb
+++ app/models/amiibo.rb
@@ -2,4 +2,6 @@ class Amiibo < ApplicationRecord
 	belongs_to :game_series
 	belongs_to :character
 	belongs_to :amiibo_series
+
+	validates :series_id, :character_id, :game_id, :image, :type, :name, presence: true
 end
