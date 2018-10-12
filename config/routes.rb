37e89acfbdce72e6b120e4118ff0c107c78c0c8a
Balache404 <morgancuell@shaw.ca>
2018-10-12 16:28:58 +0000

    Search routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :search, only: [:index] do
+  	collection do
+  		get 'results'
+  	end
+  end
+
   get 'about', to: 'amiibo#about', as: 'about'
   get 'amiibo', to: 'amiibo#list', as: 'amiibo_list'
   get 'amiibo/:id', to: 'amiibo#show', as: 'amiibo'
